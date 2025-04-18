dec1e510c53b4959b85b213be8fe463b795bbe49
J-millar99 <wlwogus33@gmail.com>
2025-04-18 21:42:30 +0900

    각 page별 고용정보 가져오기

--- scraper/job_details_parser.py
+++ scraper/job_details_parser.py
@@ -3,12 +3,14 @@ import requests, bs4
 # BeautifulSoup 클래스 import
 from bs4 import BeautifulSoup
 
-def get_total_page(count_str):
+req_header = { "user-agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'}
+
+def calculate_page_count(count_str):
     count = int(count_str.replace(",", ""))
     page = ((count - 1) // 30) + 1
     return page
 
-def calculate_page_count(occ2_id_dict):
+def get_page_url_list(occ2_id_dict):
     count_url = "https://job.incruit.com/s_common/searchjob/v3/searchjob_getcount_ajax.asp?occ2="
     url = "https://job.incruit.com/jobdb_list/searchjob.asp?articlecount=30&occ2="
 
@@ -17,11 +19,34 @@ def calculate_page_count(occ2_id_dict):
         res = requests.get(count_url + occ2_id)
         if res.ok:
             page_url = url + occ2_id + "&page="
-            page_num = get_total_page(res.text)
+            page_num = calculate_page_count(res.text)
             for page_index in range(1, page_num + 1):
-                page_list.append(page_url + str(page_num))
+                page_list.append(page_url + str(page_index))
+        else:
+            print(f"에러 코드 = {res.status_code}")
     return page_list
 
-def classify_jobs(soup, occ2_id_dict):
-    page_list = calculate_page_count(occ2_id_dict)
-    print(page_list)
+def first_cell_parser(li_tag_list):
+    for li_tag in li_tag_list:
+        first_cell = li_tag.find('div.cell_first') # 기업 이름과 태그
+        company = first_cell.find('a')
+        print(company.text)
+
+def mid_cell_parser(li_tag_list):
+    for li_tag in li_tag_list:
+        mid_cell = li_tag.find('div.cell_mid') # 채용 공고
+        title = mid_cell.find('a')
+        print(title.text)
+
+def classify_jobs(occ2_id_dict):
+    page_url_list = get_page_url_list(occ2_id_dict)
+    for page_url in page_url_list[:2]:
+        res = requests.get(page_url, headers=req_header)
+        if res.ok:
+            html = res.text
+            soup = BeautifulSoup(html, "html.parser")
+            li_tag_list = soup.select("li.c_row")
+            first_cell_parser(li_tag_list)
+            mid_cell_parser(li_tag_list)
+        else:
+            print(f"에러 코드 = {res.status_code}")
