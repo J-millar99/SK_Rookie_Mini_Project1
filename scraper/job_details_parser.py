168a138f3864aa42caf4be72f65e9d87169a2d88
J-millar99 <wlwogus33@gmail.com>
2025-04-18 10:18:13 +0900

    대분류 직무에서 세부분류 직무까지 page_url_list 생성

--- scraper/job_details_parser.py
+++ scraper/job_details_parser.py
@@ -3,14 +3,25 @@ import requests, bs4
 # BeautifulSoup 클래스 import
 from bs4 import BeautifulSoup
 
-def classify_jobs(soup, occ2_id_dict):
-    url = "https://job.incruit.com/jobdb_list/searchjob.asp?occ2="
-    total = "https://job.incruit.com/s_common/searchjob/v3/searchjob_getcount_ajax.asp?occ2="
-    
+def get_total_page(count_str):
+    count = int(count_str.replace(",", ""))
+    page = ((count - 1) // 30) + 1
+    return page
+
+def calculate_page_count(occ2_id_dict):
+    count_url = "https://job.incruit.com/s_common/searchjob/v3/searchjob_getcount_ajax.asp?occ2="
+    url = "https://job.incruit.com/jobdb_list/searchjob.asp?articlecount=30&occ2="
+
+    page_list = []
     for occ2_id in occ2_id_dict.keys():
-        detail_job = dict()
-        res1 = requests.get(url + occ2_id)
-        res2 = requests.get(total + occ2_id)
-        if res2.ok:
-            data = res2.text
-            print(data)
+        res = requests.get(count_url + occ2_id)
+        if res.ok:
+            page_url = url + occ2_id + "&page="
+            page_num = get_total_page(res.text)
+            for page_index in range(1, page_num + 1):
+                page_list.append(page_url + str(page_num))
+    return page_list
+
+def classify_jobs(soup, occ2_id_dict):
+    page_list = calculate_page_count(occ2_id_dict)
+    print(page_list)
